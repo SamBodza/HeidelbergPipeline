0a6c6c62e65c959999a3dabba31354e9b4fc0639
Bonsa11 <sambodza@hotmail.co.uk>
2022-07-14 10:56:45 +0100

    chnages

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Script to keep PostgreSQL Databases up to date
+import os
+from datetime import date
+
 from config_parser import get_config
 from update_live_dir import update_live_db
 from rsync_folders import rsync_folders_for_time
@@ -7,7 +10,7 @@ from create_logger import create_logger
 
 def main():
     config = get_config()
-    logger = create_logger(config.get('LOGGING', 'LOGGING_PATH'),
+    logger = create_logger(os.path.join(config.get('LOGGING', 'LOGGING_PATH'), f'{date.today()}.log'),
                            __file__,
                            config.get('LOGGING', 'LOGGING_LEVEL'))
     update_live_db(logger, config.get('PATHS', 'src_dir'), '.pat')
